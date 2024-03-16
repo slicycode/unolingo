bd9c8b4c2cb48eca73c54bb03afc3e6ddb747a69
slicycode <deparisjules180@gmail.com>
2024-03-16 15:38:44 +0100

    chore: add clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 
 const font = Nunito({ subsets: ['latin'] })
@@ -15,10 +16,12 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>
-        <main>{children}</main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
