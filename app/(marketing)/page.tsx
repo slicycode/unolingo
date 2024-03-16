d39b7d3825d2cd1fbf20f5d3458c47ac134f066e
slicycode <deparisjules180@gmail.com>
2024-03-16 15:58:35 +0100

    feat(auth): add auth with Clerk + landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,59 @@
 import { Button } from '@/components/ui/button'
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+} from '@clerk/nextjs'
+import { Loader } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="mx-auto flex flex-col space-y-4 p-4">
-      <Button>Hello world</Button>
+    <div className="mx-auto flex w-full max-w-[988px] flex-1 flex-col items-center justify-center gap-2 p-4 lg:flex-row">
+      <div className="lb:mb-0 relative mb-8 h-[240px] w-[240px] lg:h-[424px] lg:w-[424px]">
+        <Image src="/hero.svg" fill alt="Hero Section" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="max-w-[480px] text-center text-xl font-bold text-neutral-600 lg:text-3xl">
+          Learn, practice, and master new languages with Unolingo.
+        </h1>
+        <div className="flex flex-col items-center gap-2">
+          <ClerkLoading>
+            <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn"
+              >
+                <Button variant="secondary" className="w-full" size="lg">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn"
+              >
+                <Button variant="primaryOutline" className="w-full" size="lg">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" className="w-full" variant="secondary" asChild>
+                <Link href="/learn">Continue Learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
     </div>
   )
 }
