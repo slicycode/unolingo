9186eb3068ef6431a322eeef47261a1cd11625c2
slicycode <deparisjules180@gmail.com>
2024-03-16 15:25:04 +0100

    feat(design-system): add custom button variants

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,19 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div className="flex flex-col space-y-4 p-4">
-      <Button>Primary</Button>
-      <Button>Primary Outline</Button>
+    <div className="mx-auto flex max-w-52 flex-col space-y-4 p-4">
+      <Button>Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="primaryOutline">Primary Outline</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="secondaryOutline">Secondary Outline</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="dangerOutline">Danger Outline</Button>
+      <Button variant="super">Super</Button>
+      <Button variant="superOutline">Super Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="sidebar">Sidebar</Button>
+      <Button variant="sidebarOutline">Sidebar Outline</Button>
     </div>
   )
 }
