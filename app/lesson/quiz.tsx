ecbff96d0eb5d69d0cf8dbc1bcd9b538ed8f3d3c
slicycode <deparisjules180@gmail.com>
2024-03-18 16:41:30 +0100

    feat: add challenges card on the lesson page

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,8 +1,12 @@
 'use client'
 
 import { challengeOptions, challenges } from '@/database/schema'
-import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { useState, useTransition } from 'react'
+import { Challenge } from './challenge'
+import { Footer } from './footer'
 import { Header } from './header'
+import { QuestionBubble } from './question-bubble'
 
 type Props = {
   initialPercentage: number
@@ -22,8 +26,40 @@ export const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
+  const router = useRouter()
+  const [pending, startTransition] = useTransition()
+  const [lessonId] = useState(initialLessonId)
   const [hearts, setHearts] = useState(initialHearts)
-  const [percentage, setPercentage] = useState(initialPercentage)
+  const [percentage, setPercentage] = useState(() => {
+    return initialPercentage === 100 ? 0 : initialPercentage
+  })
+  const [challenges] = useState(initialLessonChallenges)
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed,
+    )
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex
+  })
+  const [selectedOption, setSelectedOption] = useState<number>()
+  const [status, setStatus] = useState<'correct' | 'wrong' | 'none'>('none')
+
+  const challenge = challenges[activeIndex]
+  const options = challenge?.challengeOptions ?? []
+
+  const title =
+    challenge.type === 'ASSIST'
+      ? 'Select the correct meaning'
+      : challenge.question
+
+  const onNext = () => {
+    setActiveIndex((current) => current + 1)
+  }
+
+  const onSelect = (id: number) => {
+    if (status !== 'none') return
+
+    setSelectedOption(id)
+  }
 
   return (
     <>
@@ -31,6 +67,33 @@ export const Quiz = ({
         hearts={hearts}
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
+      />
+      <div className="flex-1">
+        <div className="flex h-full items-center justify-center">
+          <div className="flex w-full flex-col gap-y-12 px-6 lg:min-h-[350px] lg:w-[600px] lg:px-0">
+            <h1 className="text-center text-lg font-bold text-neutral-700 lg:text-start lg:text-3xl">
+              {title}
+            </h1>
+            <div>
+              {challenge.type === 'ASSIST' && (
+                <QuestionBubble question={challenge.question} />
+              )}
+              <Challenge
+                options={options}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
+                disabled={pending}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer
+        disabled={pending || !selectedOption}
+        status={status}
+        onCheck={() => {}}
       />
     </>
   )
