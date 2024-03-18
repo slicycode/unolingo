81c967c51347375dd48eb985e63e405e369d1426
slicycode <deparisjules180@gmail.com>
2024-03-18 11:07:50 +0100

    feat: fetch and add course progress in units

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,18 +1,33 @@
 import { FeedWrapper } from '@/components/feed-wrapper'
 import { StickyWrapper } from '@/components/sticky-wrapper'
 import { UserProgress } from '@/components/user-progress'
-import { getUnits, getUserProgress } from '@/database/queries'
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+} from '@/database/queries'
 import { redirect } from 'next/navigation'
 import { Header } from './header'
 import { Unit } from './unit'
 
 export default async function LearnPage() {
   const userProgressData = getUserProgress()
+  const courseProgressData = getCourseProgress()
+  const lessonPercentageData = getLessonPercentage()
   const unitsData = getUnits()
 
-  const [userProgress, units] = await Promise.all([userProgressData, unitsData])
+  const [userProgress, units, courseProgress, lessonPercentage] =
+    await Promise.all([
+      userProgressData,
+      unitsData,
+      courseProgressData,
+      lessonPercentageData,
+    ])
 
   if (!userProgress || !userProgress.activeCourse) redirect('/courses')
+
+  if (!courseProgress) redirect('/courses')
 
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
@@ -34,8 +49,8 @@ export default async function LearnPage() {
             order={unit.order}
             description={unit.description}
             lessons={unit.lessons}
-            activeLesson={undefined}
-            activeLessonPercentage={0}
+            activeLesson={courseProgress.activeLesson}
+            activeLessonPercentage={lessonPercentage}
           />
         ))}
       </FeedWrapper>
